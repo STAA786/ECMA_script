9f7e1837b8aa345182423badc706d27300749820
TAJAMMUL S <human3330@gmail.com>
2021-07-15 00:01:26 +0530

    main

--- areaofrect.js
+++ areaofrect.js
@@ -12,5 +12,9 @@ console.log(`AreaOfCircle is ${AreaOfCir(50)} sq. unit`);
 const x=10;
 const b=15;
 const h=30;
+const r1=7;
+const r2=5;
+
 
 console.log(`Area of Diagram III is ${AreaOfTraingle(x,b) + AreaOfTraingle(b,h -x)}`);
+console.log(`Area of Diagram V is  ${AreaOfRect(b-r1,h) + AreaOfTraingle(b-r1,x) + AreaOfCir(r1)/2 - AreaOfCir(r2)}`);
--- newassing.js
+++ newassing.js
@@ -1,21 +1,15 @@
 
-const AreaOfTraingle= (b, h) => (1/2) * b * h;
 const AreaOfRect= (b, w) => b * w;
-const AreaOfCir=(r) => (1/2)(22/7) * r* r;
+const AreaOfTraingle= (b, h) => (1/2) * b * h;
 const AreaOfCir=(r) => (22/7) * r* r;
 
-console.log(`Area of traingle is ${AreaOfTraingle(10,20)}sq. unit`)
-console.log(`AreaOfRect is ${AreaOfRect(20,10)} sq. unit)`)
-console.log(`Area of traingle is ${AreaOfTraingle(10,20)}sq. unit`)
-console.log(`AreaOfCircle is ${AreaOfCir(50)} sq. unit`);
+console.log(`AreaOfRect is ${AreaOfRect(20,10)} sq. unit`)
+console.log(`Area of traingle is ${AreaOfTraingle(11,26)} sq. unit`);
+console.log(`AreaOfCircle is ${AreaOfCir(5)} sq. unit`);
 
-const h= 14;
-const b= 27;
 const x= 12;
+const b= 27;
+const h= 14;
 const r= 7;
 const r2= 5;
-
-
-
-
-console.log(`Area of Traingle ${AreaOfRect(b-r,h) + AreaOfTraingle(b-r,h) + AreaOfCir`)
+console.log(`Area of Diagram V is  ${AreaOfRect(b-r1,h) + AreaOfTraingle(b-r1,x) + AreaOfCir(r1)/2 - AreaOfCir(r2)}`);
